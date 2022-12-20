776d4349ee52b13d784f16e3fc8fec2d6f8d05ca
VikitorChidi <victorblessed9@gmail.com>
2022-12-20 13:49:48 +0100

    new file

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 export default function getSumOfHoods(
   initialNumber,
   expansion1989 = 89,
   expansion2019 = 19
 ) {
-=======
-export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
->>>>>>> ddaee900792b1e5cf663e02d87717d029e89171d
   return initialNumber + expansion1989 + expansion2019;
 }
